f883a61e4fb3fd554412bad90ce56cccfeac08a4
Axel Tetzlaff <axel.tetzlaff@shopify.com>
2024-04-13 16:01:00 -0400

    fix assets bundling

--- examples/bin/bundle.sh
+++ examples/bin/bundle.sh
@@ -6,4 +6,4 @@ $(dirname $0)/build.sh
 mkdir $project_root/dist
 cp $project_root/main.js $project_root/dist
 cp $project_root/index.html $project_root/dist
-cp -r $project_root/css $project_root/dist
+cp $project_root/picomponents.js $project_root/dist/
